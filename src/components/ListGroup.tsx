271967f66936e1872ece508dbf198af8a7007807
sahaann <gowdasahan13@gmail.com>
2023-06-08 10:02:44 +0530

    react-app with states, handling events, dynamic rendering

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function ListGroup() {
+  let items = ["Bengaluru", "Chennai", "Mumbai", "Kochi"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item Found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
