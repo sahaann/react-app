9a545299e3eb6363dbef7958a2cba6501f626c22
sahaann <gowdasahan13@gmail.com>
2023-06-08 10:17:38 +0530

    Added Alert, new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 function App() {
   let items = ["Bengaluru", "Chennai", "Mumbai", "Kochi"];
-
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup items={items} heading={"Cities"} />
+      <ListGroup
+        items={items}
+        heading={"Cities"}
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
