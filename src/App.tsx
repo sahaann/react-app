18560d2c96c7d27c722175ebfdfe38ae8d6703ca
sahaann <gowdasahan13@gmail.com>
2023-06-08 10:50:56 +0530

    button props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button color="danger" onClick={() => console.log("clicked")}>
+        My Button
+      </Button>
     </div>
   );
 }
